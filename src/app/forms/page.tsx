0b29399fb2e06803fb1a32db0c35e4936937053a
Sebas de la Torre <lucasebasdlt@gmail.com>
2025-05-06 11:06:17 -0300

    nuevo helper checksession y funcion en vista forms para redireccionar si estas loggeado

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -1,10 +1,26 @@
 'use client'
 import {FormLogin} from "@/components/FormLogin"
 import {FormRegister} from "@/components/FormRegister"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import {useRouter} from "next/navigation"
+import { checkSession } from "@/helpers/checkSession"
 
 export default function Forms(){
     const [toggle, setToggle] = useState<boolean>(false);
+    const router = useRouter();
+
+
+
+    useEffect(() => {
+        const checkLogged = async ()=>{
+           const isLogged = await checkSession()
+           if(isLogged){
+                 router.push("/");
+           }
+        }
+checkLogged()
+       
+    }, [])
 
     return(
         <main className="mt-16 font-afacad ">
